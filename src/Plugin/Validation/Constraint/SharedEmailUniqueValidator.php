5468ca23f25495bfcf39f7ff2928a4573a6cc964
luckyman1004 <luckysuperdev@gmail.com>
2021-12-23 10:39:31 +0200

    ref #3255764 - Implement dependency injection

--- src/Plugin/Validation/Constraint/SharedEmailUniqueValidator.php
+++ src/Plugin/Validation/Constraint/SharedEmailUniqueValidator.php
@@ -3,14 +3,55 @@
 namespace Drupal\sharedemail\Plugin\Validation\Constraint;
 
 use Drupal\Core\Validation\Plugin\Validation\Constraint\UniqueFieldValueValidator;
+use Drupal\Core\Config\ConfigFactory;
+use Drupal\Core\Session\AccountInterface;
 use Symfony\Component\Validator\Constraint;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class SharedEmailUniqueValidator.
  *
  * @package Drupal\sharedemail\Plugin\Validation\Constraint
  */
-class SharedEmailUniqueValidator extends UniqueFieldValueValidator {
+class SharedEmailUniqueValidator extends UniqueFieldValueValidator implements ContainerInjectionInterface {
+
+  /**
+   * Provides config.factory service.
+   *
+   * @var \Drupal\Core\Config\ConfigFactory
+   */
+  protected $config;
+
+  /**
+   * Returns the current_user service.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $currentUser;
+
+  /**
+   * Constructs a new SharedEmailUniqueValidator.
+   *
+   * @param \Drupal\Core\Config\ConfigFactory $config
+   *   Defines the configuration object factory.
+   * @param \Drupal\Core\Session\AccountInterface $current_user
+   *   Defines an account interface which represents the current user.
+   */
+  public function __construct(ConfigFactory $config, AccountInterface $current_user) {
+    $this->config = $config;
+    $this->currentUser = $current_user;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory'),
+      $container->get('current_user')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -19,8 +60,8 @@ class SharedEmailUniqueValidator extends UniqueFieldValueValidator {
     if (!$item = $items->first()) {
       return;
     }
-    if (\Drupal::currentUser()->getAccount()->hasPermission('create shared email account')) {
-      $allowed = \Drupal::config('sharedemail.settings')->get('sharedemail_allowed');
+    if ($this->currentUser->hasPermission('create shared email account')) {
+      $allowed = $this->config->getEditable('sharedemail.settings')->get('sharedemail_allowed');
       if (empty($allowed) || stripos($allowed, $item->value) !== FALSE) {
         return;
       }
